b78328351d95c66eb7ce5e997c7789a1b4e777d1
hug20003 <104556423+hug20003@users.noreply.github.com>
2022-11-16 18:44:44 +0100

    Add files via upload

--- InterfaceProjet/main.c
+++ InterfaceProjet/main.c
@@ -13,6 +13,7 @@ int main() {
         exit(EXIT_FAILURE); }
     show_mouse(screen);
 
+
     BITMAP* buffer;
     BITMAP* page;
     BITMAP* page2;
@@ -24,9 +25,24 @@ int main() {
     page2= create_bitmap(1324,1068);
     clear_bitmap(page2);
 
+    textprintf_ex(page,font,2,1070,(255,255,255),-1,"Vos informations de jeu: ");
     rectfill(page2,0,0,1324,1068,(0,255,0));
+    ellipse(page,50,1100,50,10,(0,0,0));
+    rectfill(page, 52,1102,100,1110,(255,255,255));
+    textprintf_ex(page,font,52,1102,(255,255,255),-1,"Argent: %d",);
 
+    ellipse(page,300,1100,50,10,(0,0,0));
+    rectfill(page, 302,1102,350,1110,(255,255,255));
+    textprintf_ex(page,font,302,1102,(255,255,255),-1,"Nb habitants: %d",);
 
+    ellipse(page,550,1100,50,10,(0,0,0));
+    rectfill(page, 552,1102,600,1110,(255,255,255));
+    textprintf_ex(page,font,552,1102,(255,255,255),-1,"Capa eau: %d",);
+
+    ellipse(page,800,1100,50,10,(0,0,0));
+    rectfill(page, 802,1102,850,1110,(255,255,255));
+    textprintf_ex(page,font,52,1102,(255,255,255),-1,"Capa elec: %d",);
+    
 
     blit(buffer,page,0,0,0,0,1024,768);
     blit(page2,page,0,0,0,0,1324,1068);
