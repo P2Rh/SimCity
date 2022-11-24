49dd4e5bdd9d3eecad941af9bc9e4e5d483be679
clement <clement.bernard@edu.ece.fr>
2022-11-24 10:15:45 +0100

    Menu principal

--- maBiblio.h
+++ maBiblio.h
@@ -11,7 +11,30 @@
 
 /*----------------------------------------------*/
 ///Structures
+typedef struct ressources{
+    int argent;
+    int nbHabitants;
+    int capaElec;
+    int capaEau;
+}t_ressources;
 
+typedef struct element{ // Infos du terrain
+    int typeElement; // 1= habitation 2= chateau eau, 3=central élec, 4=caserne 5=route
+    int nivTerrain; // son niveau (ex: niveau 5= gratte ciel)
+    int posX; // num ligne
+    int posY; //num colonne
+    int dimX;
+    int dimY;
+    int prix;
+    int coRoute;/// 1 si connecté, 0 si non
+    int coRouteX;
+    int coRouteY;
+    int distrib; /// quantité d'eau que le chateau ou la centrale distribue
+    int aproElec; ///taux d'approvisionnement élec qui dépend du niveau de l'habitation
+    int aproEau; ///taux d'approvisionnement eau qui va depend du niveau de l'habitation
+    int usineViste;///0 ou 1
+    int maisonVisite;///0 ou 1
+}t_element;
 
 
 /*----------------------------------------------*/
